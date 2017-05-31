0b3500f283ef9e9e254a6cfd83bb8380d0680318
Ben Schoeggl <benschoeggl@gmail.com>
2017-05-31 10:55:06 -0700

    Combined textViews into one double array implemented shift functionality

--- app/src/main/java/com/example/ben/atomkey/MainActivity.java
+++ app/src/main/java/com/example/ben/atomkey/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.ben.atomkey;
 
-import android.graphics.Color;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -31,11 +30,6 @@ public class MainActivity extends AppCompatActivity {
     private static final double ROW_E_LEFT_OFFSET = 2.0;
 
     private TextView[][] textViews;
-    private TextView[] textViewsA;
-    private TextView[] textViewsB;
-    private TextView[] textViewsC;
-    private TextView[] textViewsD;
-    private TextView[] textViewsE;
     private ScrollView vScrollView;
     private HorizontalScrollView hScrollView;
     private LinearLayout linearLayoutRowA;
@@ -43,12 +37,13 @@ public class MainActivity extends AppCompatActivity {
     private LinearLayout linearLayoutRowC;
     private LinearLayout linearLayoutRowD;
     private LinearLayout linearLayoutRowE;
-    private LinearLayout linearLayoutKeyboard;
+    private LinearLayout letterKeyboard;
 
     private TextView currentHighlightedKey;
     private int currentRow;
     private int currentColumn;
     private EditText field;
+    private boolean isUpperCase;
 
     private Button enterTextButton;
     private Button deleteButton;
@@ -117,20 +112,25 @@ public class MainActivity extends AppCompatActivity {
         setOffsets();
         setTextViewSize(KEY_SIZE);
         setBackgrounds();
-        linearLayoutKeyboard.setPadding(300, 300, 300, 300);
+        letterKeyboard.setPadding(300, 300, 300, 300);
         currentRow = -1;
         currentColumn = -1;
+        setCase(false);
         updateHighlightedKey();
     }
 
     private void processEnterEvent() {
         if (currentHighlightedKey != null) {
             String tag = currentHighlightedKey.getTag().toString();
-            if (tag.length() == 1)
-                field.setText(field.getText().toString() + tag);
+            if (tag.length() == 1) {
+                field.setText(field.getText().toString() +
+                        (isUpperCase ? tag.toUpperCase() : tag.toLowerCase()));
+                if (isUpperCase)
+                    setCase(false);
+            }
             else if (tag.equalsIgnoreCase("shift"))
             {
-                //do shift stuff
+                setCase(!isUpperCase);
             }
             else if (tag.equalsIgnoreCase("sym"))
             {
@@ -161,10 +161,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView getViewUnderCenter()
     {
-        int centerScrollY = vScrollView.getScrollY() + vScrollView.getHeight() / 2 - linearLayoutKeyboard.getPaddingLeft(); // all padding should be the same
+        int centerScrollY = vScrollView.getScrollY() + vScrollView.getHeight() / 2 - letterKeyboard.getPaddingLeft(); // all padding should be the same
         int row = centerScrollY < 0 || centerScrollY > NUM_ROWS * KEY_SIZE ? -1 : centerScrollY / KEY_SIZE;
-        TextView[] viewArrayRow = null;
-        int centerScrollX = (hScrollView.getScrollX() - linearLayoutKeyboard.getPaddingLeft()) + hScrollView.getWidth() / 2;
+        int centerScrollX = (hScrollView.getScrollX() - letterKeyboard.getPaddingLeft()) + hScrollView.getWidth() / 2;
         double keyLengths = (centerScrollX / (double) KEY_SIZE);
         int column = -1;
         switch(row)
@@ -172,43 +171,40 @@ public class MainActivity extends AppCompatActivity {
             case 0:
                 if (keyLengths > ROW_A_LEFT_OFFSET && keyLengths < ROW_A_LEFT_OFFSET + ROW_A_SIZE)
                     column = (int) Math.floor(keyLengths - ROW_A_LEFT_OFFSET);
-                viewArrayRow = textViewsA;
                 break;
             case 1:
                 if (keyLengths > ROW_B_LEFT_OFFSET && keyLengths < ROW_B_LEFT_OFFSET + ROW_B_SIZE)
                     column = (int) Math.floor(keyLengths - ROW_B_LEFT_OFFSET);
-                viewArrayRow = textViewsB;
                 break;
             case 2:
                 if (keyLengths > ROW_C_LEFT_OFFSET && keyLengths < ROW_C_LEFT_OFFSET + ROW_C_SIZE)
                     column = (int) Math.floor(keyLengths - ROW_C_LEFT_OFFSET);
-                viewArrayRow = textViewsC;
                 break;
             case 3:
                 if (keyLengths > ROW_D_LEFT_OFFSET && keyLengths < ROW_D_LEFT_OFFSET + ROW_D_SIZE)
                     column = (int) Math.floor(keyLengths - ROW_D_LEFT_OFFSET);
-                viewArrayRow = textViewsD;
                 break;
             case 4:
                 if (keyLengths > ROW_E_LEFT_OFFSET && keyLengths < ROW_E_LEFT_OFFSET + ROW_E_SIZE)
                     column = (int) Math.floor(keyLengths - ROW_E_LEFT_OFFSET);
-                viewArrayRow = textViewsE;
                 break;
         }
+
         currentColumn = column;
         currentRow = row;
-        if (viewArrayRow != null && (column > -1 && column < viewArrayRow.length))
-            return viewArrayRow[column];
+        if (row > -1 && row < NUM_ROWS && column > -1 && column < textViews[row].length)
+            return textViews[row][column];
         else
             return null;
     }
 
     private void findViews() {
-        textViewsA = new TextView[ROW_A_SIZE];
-        textViewsB = new TextView[ROW_B_SIZE];
-        textViewsC = new TextView[ROW_C_SIZE];
-        textViewsD = new TextView[ROW_D_SIZE];
-        textViewsE = new TextView[ROW_E_SIZE];
+        textViews = new TextView[NUM_ROWS][];
+        textViews[0] = new TextView[ROW_A_SIZE];
+        textViews[1] = new TextView[ROW_B_SIZE];
+        textViews[2] = new TextView[ROW_C_SIZE];
+        textViews[3] = new TextView[ROW_D_SIZE];
+        textViews[4] = new TextView[ROW_E_SIZE];
         hScrollView = (HorizontalScrollView) findViewById(R.id.hScrollView);
         vScrollView = (ScrollView) findViewById(R.id.vScrollView);
         linearLayoutRowA = (LinearLayout) findViewById(R.id.RowA);
@@ -216,7 +212,7 @@ public class MainActivity extends AppCompatActivity {
         linearLayoutRowC = (LinearLayout) findViewById(R.id.RowC);
         linearLayoutRowD = (LinearLayout) findViewById(R.id.RowD);
         linearLayoutRowE = (LinearLayout) findViewById(R.id.RowE);
-        linearLayoutKeyboard = (LinearLayout) findViewById(R.id.keyboard);
+        letterKeyboard = (LinearLayout) findViewById(R.id.keyboard);
         textViews[0][0] = (TextView) findViewById(R.id.A1);
         textViews[0][1] = (TextView) findViewById(R.id.A2);
         textViews[0][2] = (TextView) findViewById(R.id.A3);
@@ -246,36 +242,6 @@ public class MainActivity extends AppCompatActivity {
         textViews[4][2] = (TextView) findViewById(R.id.E3);
         textViews[4][3] = (TextView) findViewById(R.id.E4);
         textViews[4][4] = (TextView) findViewById(R.id.E5);
-
-//        textViewsA[0] = (TextView) findViewById(R.id.A1);
-//        textViewsA[1] = (TextView) findViewById(R.id.A2);
-//        textViewsA[2] = (TextView) findViewById(R.id.A3);
-//        textViewsA[3] = (TextView) findViewById(R.id.A4);
-//        textViewsB[0] = (TextView) findViewById(R.id.B1);
-//        textViewsB[1] = (TextView) findViewById(R.id.B2);
-//        textViewsB[2] = (TextView) findViewById(R.id.B3);
-//        textViewsB[3] = (TextView) findViewById(R.id.B4);
-//        textViewsB[4] = (TextView) findViewById(R.id.B5);
-//        textViewsB[5] = (TextView) findViewById(R.id.B6);
-//        textViewsC[0] = (TextView) findViewById(R.id.C1);
-//        textViewsC[1] = (TextView) findViewById(R.id.C2);
-//        textViewsC[2] = (TextView) findViewById(R.id.C3);
-//        textViewsC[3] = (TextView) findViewById(R.id.C4);
-//        textViewsC[4] = (TextView) findViewById(R.id.C5);
-//        textViewsC[5] = (TextView) findViewById(R.id.C6);
-//        textViewsC[6] = (TextView) findViewById(R.id.C7);
-//        textViewsD[0] = (TextView) findViewById(R.id.D1);
-//        textViewsD[1] = (TextView) findViewById(R.id.D2);
-//        textViewsD[2] = (TextView) findViewById(R.id.D3);
-//        textViewsD[3] = (TextView) findViewById(R.id.D4);
-//        textViewsD[4] = (TextView) findViewById(R.id.D5);
-//        textViewsD[5] = (TextView) findViewById(R.id.D6);
-//        textViewsD[6] = (TextView) findViewById(R.id.D7);
-//        textViewsE[0] = (TextView) findViewById(R.id.E1);
-//        textViewsE[1] = (TextView) findViewById(R.id.E2);
-//        textViewsE[2] = (TextView) findViewById(R.id.E3);
-//        textViewsE[3] = (TextView) findViewById(R.id.E4);
-//        textViewsE[4] = (TextView) findViewById(R.id.E5);
         field = (EditText) findViewById(R.id.field);
         enterTextButton = (Button) findViewById(R.id.inputButton);
         deleteButton = (Button) findViewById(R.id.deleteButton);
@@ -293,53 +259,41 @@ public class MainActivity extends AppCompatActivity {
 
     private void setTextViewSize(int size)
     {
-        for (int i = 0; i < ROW_A_SIZE; i++)
-        {
-            textViewsA[i].getLayoutParams().width = size;
-            textViewsA[i].getLayoutParams().height = size;
-        }
-        for (int i = 0; i < ROW_B_SIZE; i++)
-        {
-            textViewsB[i].getLayoutParams().width = size;
-            textViewsB[i].getLayoutParams().height = size;
-        }
-        for (int i = 0; i < ROW_C_SIZE; i++)
-        {
-            textViewsC[i].getLayoutParams().width = size;
-            textViewsC[i].getLayoutParams().height = size;
-        }
-        for (int i = 0; i < ROW_D_SIZE; i++)
-        {
-            textViewsD[i].getLayoutParams().width = size;
-            textViewsD[i].getLayoutParams().height = size;
-        }
-        for (int i = 0; i < ROW_E_SIZE; i++)
-        {
-            textViewsE[i].getLayoutParams().width = size;
-            textViewsE[i].getLayoutParams().height = size;
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < textViews[i].length; j++)
+            {
+                textViews[i][j].getLayoutParams().width = size;
+                textViews[i][j].getLayoutParams().height = size;
+            }
         }
     }
 
     private void setBackgrounds() {
-        for (int i = 0; i < ROW_A_SIZE; i++)
-        {
-            textViewsA[i].setBackgroundResource(R.drawable.background_top_normal);
+        for (int i = 0; i < NUM_ROWS; i++) {
+            for (int j = 0; j < textViews[i].length; j++) {
+                int id = i == NUM_ROWS - 1 ? R.drawable.background_top_normal : R.drawable.background_top_bottom_normal;
+                textViews[i][j].setBackgroundResource(
+                        i == NUM_ROWS - 1
+                                ? R.drawable.background_top_normal
+                                : R.drawable.background_top_bottom_normal
+                );
+            }
         }
-        for (int i = 0; i < ROW_B_SIZE; i++)
-        {
-            textViewsB[i].setBackgroundResource(R.drawable.background_top_normal);
+    }
+
+    private void setCase(boolean upper)
+    {
+        for (int i = 0; i < textViews.length; i++) {
+            for (int j = 0; j < textViews[i].length; j++) {
+                String tag = textViews[i][j].getTag().toString();
+                if (tag.length() == 1) {
+                    if (upper)
+                        textViews[i][j].setText(tag.toUpperCase());
+                    else
+                        textViews[i][j].setText(tag.toLowerCase());
+                }
+            }
         }
-        for (int i = 0; i < ROW_C_SIZE; i++)
-        {
-            textViewsC[i].setBackgroundResource(R.drawable.background_top_normal);
-        }
-        for (int i = 0; i < ROW_D_SIZE; i++)
-        {
-            textViewsD[i].setBackgroundResource(R.drawable.background_top_bottom_normal);
-        }
-        for (int i = 0; i < ROW_E_SIZE; i++)
-        {
-            textViewsE[i].setBackgroundResource(R.drawable.background_top_bottom_normal);
-        }
+        isUpperCase = upper;
     }
 }
